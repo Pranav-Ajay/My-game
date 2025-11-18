68ef7d26a463f06ee554b695672e2bd8ac53d497
Pranav-Ajay <pranavajay005@gmail.com>
2025-11-18 14:00:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = socketIo(server);
 
 app.use(express.static(__dirname));
 
-const allowedIPs = ['::1', '192.168.1.105'];
+const allowedIPs = ['::1', '2406:8800:9014:57ca:79c0:ae7b:49c3:fb89'];
 
 const players = {};
 const alphabets = 'ABCDEFGHIJKLMNOP'.split('');
