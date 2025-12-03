217d478eedb47bc5b0129d759ddfd68032e29823
Pranav-Ajay <pranavajay005@gmail.com>
2025-12-03 13:39:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,3 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
