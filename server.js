6de663c160dc1b530972ed70a7864d884ca6a6f0
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-07 19:23:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ const io = socketIo(server, { cors: { origin: "*" } });
 app.use(express.static(__dirname));
 app.use(express.json());
 
-/* ================= GAME STATE ================= */
-
-// socket.id → player object
+/
 const players = {};
 
-// 36 player slots
-const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+const alphabets = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36'
   .split('')
   .map(l => ({ letter: l, frozen: false }));
 
