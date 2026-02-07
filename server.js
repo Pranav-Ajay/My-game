0506666b2478b113340cb5e8ecc5e077180894bf
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-07 18:48:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,30 +7,12 @@ const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: "*" } });
 
 app.use(express.static(__dirname));
-app.use(express.json());   
 
 const players = {};
-let playerNames = [];   /
-
 const alphabets = 'ABCDEFGHIJKLMNOP'.split('').map(l => ({
   letter: l,
   frozen: false
 }));
-
-app.post("/add_player", (req, res) => {
-  const { name } = req.body;
-  if (!name) return res.sendStatus(400);
-
-  playerNames.push({ name });
-
-  io.emit("playersUpdated", playerNames);
-
-  res.sendStatus(200);
-});
-
-app.get("/get_players", (req, res) => {
-  res.json(playerNames);
-});
 
 io.on('connection', (socket) => {
   console.log("Player connected:", socket.id);
@@ -47,35 +29,9 @@ io.on('connection', (socket) => {
 
   players[socket.id] = { letter: assignedLetterObj.letter };
   socket.emit('assigned', players[socket.id]);
-
   io.emit('update', { players, alphabets });
 
+  // Freeze PLAYER
   socket.on('freezePlayer', ({ letter }) => {
     for (let id in players) {
-      if (players[id].letter === letter) {
-        players[id].frozen = true;
-      }
-    }
-    io.emit('update', { players, alphabets });
-  });
-
-  socket.on('unfreezePlayer', ({ letter }) => {
-    for (let id in players) {
-      if (players[id].letter === letter) {
-        players[id].frozen = false;
-      }
-    }
-    io.emit('update', { players, alphabets });
-  });
-
-  socket.on('disconnect', () => {
-    delete players[socket.id];
-    io.emit('update', { players, alphabets });
-    console.log('Player disconnected:', socket.id);
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
+      if (players
